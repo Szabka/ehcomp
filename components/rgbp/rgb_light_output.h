7c2893f09ce53cd0aeef75a98e4b1028d26613f0
Szabka <Szabka@github.com>
2025-06-12 23:37:49 +0200

    add missing power functions

--- components/rgbp/rgb_light_output.h
+++ components/rgbp/rgb_light_output.h
@@ -4,6 +4,10 @@
 #include "esphome/components/output/float_output.h"
 #include "esphome/components/light/light_output.h"
 
+#ifdef USE_POWER_SUPPLY
+#include "esphome/components/power_supply/power_supply.h"
+#endif
+
 namespace esphome {
 namespace rgbp {
 
@@ -24,12 +28,25 @@ class RGBLightOutput : public light::LightOutput {
     this->red_->set_level(red);
     this->green_->set_level(green);
     this->blue_->set_level(blue);
+#ifdef USE_POWER_SUPPLY
+    if (red>0 || green>0 || blue>0) {
+      this->power_.request();
+    } else {
+      this->power_.unrequest();
+    }
+#endif
   }
+#ifdef USE_POWER_SUPPLY
+  void set_power_supply(power_supply::PowerSupply *power_supply) { this->power_.set_parent(power_supply); }
+#endif
 
  protected:
   output::FloatOutput *red_;
   output::FloatOutput *green_;
   output::FloatOutput *blue_;
+#ifdef USE_POWER_SUPPLY
+  power_supply::PowerSupplyRequester power_;
+#endif
 };
 
 }  // namespace rgbp
